c3485154c68294c1f12d43beb804975338a76b9b
Jeorge Donato <jeorgekhenr@gmail.com>
2020-08-24 18:24:29 -0700

    Added a connection to MySQL DB

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const db = require('./db/db');
+
+
+
+db.query('SELECT * FROM department', function (error, results, fields) {
+  if (error) throw error;
+  console.log(results)
+});
+
 
--- db/db.js
+++ db/db.js
@@ -3,5 +3,16 @@ const mySql = require('mysql');
 const connection = mySql.createConnection({
   host: '127.0.0.1',
   user: 'root',
-
+  password: '12345678',
+  database: 'tracker_db'
 });
+
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
